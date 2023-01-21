4ce80128c00fe3e548ece512f8adbc17c8e1c957
jung_hyeon <j01h02@gmail.com>
2023-01-22 00:39:17 +0900

    no message

---
+++ Assets/MakePipe.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MakePipe : MonoBehaviour
+{
+    public GameObject pipe;
+    public float timeDiff;
+    float timer = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 1초마다 파이프 생성
+        timer += Time.deltaTime;
+        if (timer > timeDiff)
+        {
+            GameObject newpipe = Instantiate(pipe);
+            newpipe.transform.position = new Vector3(6,Random.Range(-3.2f, 2.35f),0);
+            timer = 0;
+            Destroy(newpipe, 10.0f);
+        }
+        
+    }
+}

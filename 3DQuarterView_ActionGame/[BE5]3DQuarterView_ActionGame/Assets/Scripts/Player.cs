6484931604802f504f64ff61dc06643006295898
jung_hyeon <j01h02@gmail.com>
2023-07-02 23:11:22 +0900

    [B39]플레이어의 점프와 회피 - 04.회피 구현

--- 3DQuarterView_ActionGame/[BE5]3DQuarterView_ActionGame/Assets/Scripts/Player.cs
+++ 3DQuarterView_ActionGame/[BE5]3DQuarterView_ActionGame/Assets/Scripts/Player.cs
@@ -11,8 +11,11 @@ public class Player : MonoBehaviour
     bool jDown;
 
     bool isJump;
+    bool isDodge;
 
     Vector3 moveVec;
+    Vector3 dodgeVec;
+
     Rigidbody rigid;
     Animator anim;
 
@@ -29,6 +32,7 @@ public class Player : MonoBehaviour
         Move();
         Turn();
         Jump();
+        Dodge();
     }
 
     private void GetInput()
@@ -44,7 +48,11 @@ public class Player : MonoBehaviour
     {
         moveVec = new Vector3(hAxis, 0, vAxis).normalized; //normalized쓰면 대각선으로 이동해도 벡터 값이 1로 보장
         // 벽은 fixedupdate이기 때문에 update문에 있는 transform이동이 무시되는 상황 발생 가능 => player의 rigidbody-collision detecion을 continuous로 변경 & WorldSpace를 static으로 변경
-
+        if (isDodge)
+        {
+            // 회피 중일때는 회피하는 방향으로 바꾸기
+            moveVec = dodgeVec;
+        }
         transform.position += moveVec * speed * (wDown ? 0.3f : 1f) * Time.deltaTime;
 
         anim.SetBool("isRun", moveVec != Vector3.zero);
@@ -58,7 +66,7 @@ public class Player : MonoBehaviour
 
     void Jump()
     {
-        if(jDown && !isJump)
+        if(jDown && moveVec == Vector3.zero && !isJump && !isDodge)
         {
             rigid.AddForce(Vector3.up * 20, ForceMode.Impulse);
             anim.SetBool("isJump", true);
@@ -66,6 +74,26 @@ public class Player : MonoBehaviour
             isJump = true;
         }
     }
+
+    void Dodge()
+    {
+        if (jDown && moveVec != Vector3.zero && !isJump && !isDodge)
+        {
+            dodgeVec = moveVec;
+            speed *= 2; // 2배의 속도로 회피
+            anim.SetTrigger("doDodge");
+            isDodge = true;
+
+            Invoke("DodgeOut", 0.5f);
+        }
+    }
+
+    void DodgeOut()
+    {
+        speed *= 0.5f;
+        isDodge = false;
+    }
+
 
     private void OnCollisionEnter(Collision collision)
     {

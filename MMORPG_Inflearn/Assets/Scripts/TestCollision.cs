9abbf5e2d0a726e8977d38f5b5e2784bec8ab90b
j01h02@gmail.com <j01h02@gmail.com>
2023-08-10 14:34:47 +0900

    [MMORPG] 섹션04. Collision (충돌) 끝

--- MMORPG_Inflearn/Assets/Scripts/TestCollision.cs
+++ MMORPG_Inflearn/Assets/Scripts/TestCollision.cs
@@ -7,15 +7,11 @@ public class TestCollision : MonoBehaviour
 
     void Update()
     {
-        // local <-> World <-> Viewport <-> Screen
-
-        /// Debug.Log(Input.mousePosition); // Screen 좌표
-
-        // Debug.Log(Camera.main.ScreenToViewportPoint(Input.mousePosition)); // Viewport
 
         if (Input.GetMouseButton(0))
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            // 위의 3줄을 한 줄로 표현하는 더 간단한 버전
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition); 
 
             Debug.DrawRay(Camera.main.transform.position, ray.direction * 100.0f, Color.red, 1.0f);
 
@@ -28,35 +24,6 @@ public class TestCollision : MonoBehaviour
                 Debug.Log($"Raycast Camera @ {hit.collider.gameObject.tag}");
             }
         }
-
-        /*
-        if(Input.GetMouseButton(0))
-        {
-            Vector3 mousePos = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, Camera.main.nearClipPlane));
-            Vector3 dir = mousePos - Camera.main.transform.position;
-            dir = dir.normalized;
-
-            Debug.DrawRay(Camera.main.transform.position, dir * 100.0f, Color.red, 1.0f);
-
-            if (Physics.Raycast(Camera.main.transform.position, dir, out hit, 100.0f))
-            {
-                Debug.Log($"Raycast Camera @ {hit.collider.gameObject.name}");
-
-            }
-        */
     }
-
-    /* 
-    Vector3 look = transform.TransformDirection(Vector3.forward);
-    Debug.DrawRay(transform.position + Vector3.up, look * 10, Color.red);
-
-    RaycastHit[] hits;
-    hits = Physics.RaycastAll(transform.position + Vector3.up, look, 10);
-
-    foreach (RaycastHit hit in hits)
-    {
-        Debug.Log($"Raycast {hit.collider.gameObject.name}");
-    }
-    */
 }
 
